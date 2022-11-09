0ce09e6aba5173355056ec975c05ff19ebf211a3
Eran Guendelman <eguendelman@fb.com>
2022-11-09 23:47:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,102 @@
+let cols, rows;
+let numChars;
+let differentCharIdx;
+let showingAnswer = false;
+
+let fontSize = 42;
+let effectiveSize = fontSize + 6;
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomCharacter() {
+    const unicodeRange = [0x1F600, 0x1F644];
+    return String.fromCodePoint(randomInteger(unicodeRange[0], unicodeRange[1]));
+}
+
+function randomCharacterExceptX(x) {
+    for (;;) {
+        let char = randomCharacter();
+        if (char != x ) { return char; }
+    }
+}
+
+function generateContent()
+{
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    cols = Math.floor(canvas.width / effectiveSize);
+    rows = Math.floor(canvas.height / effectiveSize);
+    numChars = cols*rows;
+    differentCharIdx = randomInteger(0, numChars-1);
+
+    let mainChar = randomCharacter();
+    let differentChar = randomCharacterExceptX(mainChar);
+
+    console.log(rows);
+    console.log(cols);
+
+    //ctx.font = "64px";
+    ctx.font = `${fontSize}px serif`;
+
+    let itemIdx = 0;
+    //ctx.strokeText(mainChar, 200, 200);
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            let char = (itemIdx == differentCharIdx) ? differentChar : mainChar;
+            //ctx.fillText("\u{1F600}", row*effectiveSize, col*effectiveSize + effectiveSize);
+            ctx.fillText(char, col*effectiveSize, row*effectiveSize + effectiveSize);
+            itemIdx++;
+        }
+    }
+}
+
+function showAnswer() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    let differentCharRow = Math.floor(differentCharIdx / cols);
+    let differentCharCol = differentCharIdx % cols;
+    let x = differentCharCol * effectiveSize + 0.75*fontSize;
+    let y = differentCharRow * effectiveSize + 0.75*fontSize;
+    console.log(`${x}, ${y}`);
+
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.arc(x, y, effectiveSize, 0, 2*Math.PI);
+    ctx.stroke();
+}
+
+function doNext() {
+    if (showingAnswer) {
+        showingAnswer = false;
+        generateContent();
+    } else {
+        showingAnswer = true;
+        showAnswer();
+    }
+}
+
+
+
+function initCanvas() {
+    const canvas = document.getElementById("canvas");
+    canvas.width = document.body.clientWidth; //document.width is obsolete
+    canvas.height = document.body.clientHeight; //document.height is obsolete
+}
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == "Enter") {
+        doNext();
+    }
+}, false);
+
+initCanvas();
+
+generateContent();
