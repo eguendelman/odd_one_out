409efa774d987b62c7967a9d6f934ae1df482d9f
Eran Guendelman <eguendelman@users.noreply.github.com>
2022-11-10 09:25:43 +0200

    Trying to add touch handler

--- main.js
+++ main.js
@@ -90,6 +90,8 @@ function initCanvas() {
     const canvas = document.getElementById("canvas");
     canvas.width = document.body.clientWidth; //document.width is obsolete
     canvas.height = document.body.clientHeight; //document.height is obsolete
+canvas.addEventListener('touchstart', doNext);
+
 }
 
 function changeSize(delta) {
